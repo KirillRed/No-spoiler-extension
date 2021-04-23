190da8b145f64eff6e2026917ed79e5a316f2ab1
Kirill Red <kirillr1740@gmail.com>
2021-04-23 18:24:35 +0300

    all video tags added

--- foreground.js
+++ foreground.js
@@ -11,12 +11,31 @@ try {
 catch (ReferenceError) {
 
 }
+try {
+    var videoTagList = ['ytd-type-renderer', 'ytd-grid-type-renderer', 'ytd-rich-item-renderer',
+        'ytd-playlist-type-renderer', 'ytd-playlist-panel-type-renderer', 'ytd-compact-type-renderer']; //it may be video, playlist, radio
+    var types = ['video', 'playlist', 'radio']
+}
+catch (SyntaxError) {
 
-setInterval(videoBlock, 1000)
+}
 
-function videoBlock() {
-    let videos = Array.from(document.getElementById('spinner-container').parentNode.querySelector('#contents').children);
-    for (const video of videos.slice(0, videos.length - 1)) {
+
+setInterval(firstVideoBlock, 1000)
+
+function firstVideoBlock() {
+    let videos = [];
+    for (const videoTag of videoTagList) {
+        if (videoTag.includes('type')) {
+            for (const type of types) {
+                videos = videos.concat(Array.from(document.getElementsByTagName(videoTag.replace('type', type))));
+            }
+        }
+        else {
+            videos = videos.concat(Array.from(document.getElementsByTagName(videoTag)));
+        }
+    }
+    for (const video of videos) {
         let ariaLabel;
         try {
             ariaLabel = video.querySelector('#video-title').getAttribute('aria-label');
@@ -40,7 +59,13 @@ function videoBlock() {
         if (title === null) { //It isn't video, it is channel, ads or something else
             continue;
         }
-        console.log(title, ariaLabel)
+        console.log(title)
+
     }
+    console.log(window.location.href)
+}
+
+function secondVideoBlock() {
+
 }
 
